3bc0c5851d819313c694fe309587cb5f1e328ca6
unknown <mdfaruk01720875872>
2025-06-14 10:36:57 +0600

    added an register authintikation

--- src/contex/AuthProvider.jsx
+++ src/contex/AuthProvider.jsx
@@ -1,17 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AuthContext } from './AuthContex';
-import {  createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile } from "firebase/auth";
 import { auth } from '../firebase/firebase.confige';
 
 
-const AuthProvider = ({children}) => {
-
-// register
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    // register
     const register = (email, password) => {
+        setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password)
+    };
+
+    // update profile
+    const userUpdateProfile = (userUpdate) => {
+        setLoading(true)
+        return updateProfile(auth.currentUser, { userUpdate })
     }
+
+    // logout
+    const logOut = () => {
+        return signOut(auth)
+    }
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser)
+            setLoading(false)
+        })
+        return () => {
+            unSubscribe();
+        }
+    }, [])
     const userInfo = {
-        register
+        user,
+        loading,
+        setUser,
+        register,
+        userUpdateProfile,
+        logOut
     }
     return (
         <AuthContext value={userInfo}>
--- src/shared/Navbar.jsx
+++ src/shared/Navbar.jsx
@@ -1,41 +1,92 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router';
+import UseHock from '../hock/UseHock';
+import Swal from 'sweetalert2';
 
 const Navbar = () => {
-    const Links = <>
-     <li><NavLink to='/' className={({isActive}) => isActive ? 'bg-amber-300': ''}>Home</NavLink></li>
-     <li><NavLink to='/courses' className={({isActive}) => isActive ? 'bg-amber-300' : ''}>Courses</NavLink></li>
-    </>
-    return (
-        <div>
-            <div className="navbar bg-base-100 shadow-sm px-10">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-       {Links}
-        
-      </ul>
-    </div>
-    <a className=" text-xl">Course Management 
-</a>
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-     {Links}
-    </ul>
-  </div>
-  <div className="navbar-end">
-    <Link className='btn btn-primary mr-3'>Login</Link>
-    <Link to='/register' className='btn btn-primary'>Register</Link>
-  </div>
-</div>
+  const { user, logOut } = UseHock();
+  console.log(user)
+  const Links = <>
+    <li><NavLink to='/' className={({ isActive }) => isActive ? 'bg-amber-300' : ''}>Home</NavLink></li>
+    <li><NavLink to='/courses' className={({ isActive }) => isActive ? 'bg-amber-300' : ''}>Courses</NavLink></li>
+  </>
+
+  // handle log out
+  const handleLogOut = () => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        // logOut
+        logOut()
+          .then(() => {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Your Log Out Successfully!",
+              showConfirmButton: false,
+              timer: 1500
+            });
+          })
+          .catch(error => {
+            Swal.fire({
+              position: "top-end",
+              icon: "error",
+              title: error.message,
+              showConfirmButton: false,
+              timer: 1500
+            });
+          })
+
+      }
+    });
+  }
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm px-10">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+              {Links}
+
+            </ul>
+          </div>
+          <a className=" text-xl">Course Management
+          </a>
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {Links}
+          </ul>
+        </div>
+        <div className="navbar-end">
+          {
+            user ? <>
+              <img className='w-12 h-12 rounded-full' src={user.photoURL} alt="" />
+              <button onClick={handleLogOut} className='btn btn-primary'>Log Out</button>
+            </>
+              :
+              <>
+                <Link className='btn btn-primary mr-3'>Login</Link>
+                <Link to='/register' className='btn btn-primary'>Register</Link>
+              </>
+          }
+
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
